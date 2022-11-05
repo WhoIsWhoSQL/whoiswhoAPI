fc3e42d4bf530495c5e06a79311e83d100e9785d
bernat <bernat.costa@cesurformacion.com>
2022-11-05 20:54:16 +0100

    swagger y inserts personajes

--- src/controllers/classroom.controller.js
+++ src/controllers/classroom.controller.js
@@ -113,36 +113,3 @@ exports.delete = (req, res) => {
     });
 };
 
-const Game = require("../models/game.model.js");
-
-exports.addexercise = (req,res) => {
-
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
-    console.log(req.user + req.body);
-    const pin = (Math.random() + 1).toString(36).substring(7);
-
-    const game = new Game({
-        exerciseId: req.body.exerciseId,
-        pin: pin,
-        start_date: new Date(),
-        end_date : new Date(),
-        classId :req.body.classId ,
-        teacherId: req.user.teacherId
-    });
-
-
-    Game.create(game, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the game."
-            });
-            res.send(data);
-    });
-
-
-};
